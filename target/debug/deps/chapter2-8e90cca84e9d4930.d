bbbac963e99267818153b9314901000b8ba358e4
duguorong009 <dudurong009@163.com>
2021-11-07 23:04:04 -0800

    Fix the error of "mul/div" operators.
    TODO: Implement the "exp" functionality.

--- target/debug/deps/chapter2-8e90cca84e9d4930.d
+++ target/debug/deps/chapter2-8e90cca84e9d4930.d
@@ -1,6 +1,6 @@
-/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-8e90cca84e9d4930: src/main.rs src/parsemath/mod.rs src/parsemath/ast.rs src/parsemath/parser.rs src/parsemath/token.rs src/parsemath/tokenizer.rs
+/home/wasp/Documents/GitHub/arithmetic-calculator/target/debug/deps/chapter2-8e90cca84e9d4930: src/main.rs src/parsemath/mod.rs src/parsemath/ast.rs src/parsemath/parser.rs src/parsemath/token.rs src/parsemath/tokenizer.rs
 
-/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-8e90cca84e9d4930.d: src/main.rs src/parsemath/mod.rs src/parsemath/ast.rs src/parsemath/parser.rs src/parsemath/token.rs src/parsemath/tokenizer.rs
+/home/wasp/Documents/GitHub/arithmetic-calculator/target/debug/deps/chapter2-8e90cca84e9d4930.d: src/main.rs src/parsemath/mod.rs src/parsemath/ast.rs src/parsemath/parser.rs src/parsemath/token.rs src/parsemath/tokenizer.rs
 
 src/main.rs:
 src/parsemath/mod.rs:
