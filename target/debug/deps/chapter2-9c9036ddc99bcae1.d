82f7ebcb3549e5bdc15c7c64df5393626d1f115b
duguorong009 <dudurong009@163.com>
2021-11-07 22:15:46 -0800

    Initial commit
    Arithmetic calculator inspired by book "practical system programming for rust developers".
    TODO: "*" operation does not work.

---
+++ target/debug/deps/chapter2-9c9036ddc99bcae1.d
@@ -0,0 +1,5 @@
+/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-9c9036ddc99bcae1.rmeta: src/main.rs
+
+/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-9c9036ddc99bcae1.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/chapter2-e16cb4046e925b76.d
@@ -0,0 +1,5 @@
+/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-e16cb4046e925b76.rmeta: src/main.rs
+
+/home/wasp/Documents/system-programming/chapter2/target/debug/deps/chapter2-e16cb4046e925b76.d: src/main.rs
+
+src/main.rs:
